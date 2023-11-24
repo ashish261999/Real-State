888cd724fc16082ea9a931d076475ac2079dccf0
ashish ranjan <ashishranjanmahi07@gmail.com>
2023-11-24 17:57:00 +0530

    create pages and route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@
 import './App.css';
 import Footer from './component/footer';
 import Header from './component/header';
+import {BrowserRouter , Routes , Route} from "react-router-dom";
+import Home from "./pages/home";
+import Profile from './pages/profile';
+import Signup from './pages/signup';
+import Signin from './pages/signin';
+import About from "./pages/about";
 
 function App() {
   return (
     <div>
-      <Header/>
-      <Footer/>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/sign-in' element={<Signin/>}/>
+        <Route path='/sign-up' element={<Signup />}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/about' element={<About/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
